5521d4a93f72d2f1641876bfb8e166b74b25361d
Michael Prenez-Isbell <prenezisbell@gmail.com>
2023-09-21 15:30:36 -0400

    Initial Commit

---
+++ FidelityTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FidelityTest
+//
+//  Created by Michael Prenez-Isbell on 9/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FidelityTest/FidelityTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidelityTestApp.swift
+//  FidelityTest
+//
+//  Created by Michael Prenez-Isbell on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct FidelityTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
