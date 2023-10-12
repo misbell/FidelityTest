9a40b99a430a74ebbb9459ee9b4bbf0c9e6dfe8d
Michael Prenez-Isbell <prenezisbell@gmail.com>
2023-10-12 17:59:15 -0400

    start

--- FidelityTest/ContentView.swift
+++ FidelityTest/ContentView.swift
@@ -7,18 +7,84 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State private var results = [Response]()
+
+
+    struct Response: Codable {
+        let id: Int
+        let name, username, email: String
+        let address: Address
+        let phone, website: String
+        let company: Company
+    }
+
+    // MARK: - Address
+    struct Address: Codable {
+        let street, suite, city, zipcode: String
+        let geo: Geo
+    }
+
+    // MARK: - Geo
+    struct Geo: Codable {
+        let lat, lng: String
+    }
+
+    // MARK: - Company
+    struct Company: Codable {
+        let name, catchPhrase, bs: String
+    }
+
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(results, id: \.id) { item in
+                   VStack(alignment: .leading) {
+                    
+                       Text(item.name)
+                       Text(item.username)
+                       Text(item.email)
+                   }
+               }
+            .task {
+                await loadData()
+            }
+            
         }
         .padding()
+    }
+    
+    func loadData() async {
+        // "https://itunes.apple.com/search?term=taylor+swift&entity=song"
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") 
+        else
+        {
+            print("Invalid URL")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+
+            if let decodedResponse = try? JSONDecoder().decode([Response].self, from: data) {
+            
+                results = decodedResponse
+               
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+ 
